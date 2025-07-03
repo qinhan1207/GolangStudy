bc0d468dad03f40dfbf7ba9160e9974b45e46cf1
qinhan <3091533438@qq.com>
2025-07-03 13:31:35 +0800

    refactor(03_init): 注释掉未使用的包导入
    - 注释掉了 lib1 包的两种导入方式
    - 保留了 lib2 包的导入

--- 03_init/main.go
+++ 03_init/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	// _ "github.com/qinhan1207/GolangStudy/03_init/lib1" // 表示这个包可以不被使用
+	// mylib2 "github.com/qinhan1207/GolangStudy/03_init/lib1" // 给包起别名
 	"github.com/qinhan1207/GolangStudy/03_init/lib1"
 	"github.com/qinhan1207/GolangStudy/03_init/lib2"
 )
